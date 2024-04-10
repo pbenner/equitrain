c6c5c7ff58e356da7e140d778a2ffb0e9a162f35
Philipp Benner <philipp.benner@bam.de>
2024-04-10 21:01:22 +0200

    implemented prototype for optionally excluding forces or stress from training

--- equitrain/train.py
+++ equitrain/train.py
@@ -333,63 +333,86 @@ def create_optimizer_v2(
 
 # %%
 
-def compute_weighted_loss(args, energy_loss, force_loss):
+def log_metrics(args, logger, prefix, postfix, loss_metrics):
+
+    info_str  = prefix
+    info_str += 'loss: {loss:.5f}'.format(loss=loss_metrics['total'].avg)
+
+    if args.energy_weight > 0.0:
+        info_str += ', loss_e: {loss_e:.5f}'.format(
+            loss_e=loss_metrics['energy'].avg,
+        )
+    if args.force_weight > 0.0:
+        info_str += ', loss_f: {loss_f:.5f}'.format(
+            loss_f=loss_metrics['forces'].avg,
+        )
+    if args.stress_weight > 0.0:
+        info_str += ', loss_s: {loss_f:.5f}'.format(
+            loss_f=loss_metrics['stress'].avg,
+        )
+
+    if postfix is not None:
+        info_str += postfix
+
+    logger.info(info_str)
+
+
+def compute_weighted_loss(args, energy_loss, force_loss, stress_loss):
     result = 0.0
     # handle initial values correctly when weights are zero, i.e. 0.0*Inf -> NaN
     if not math.isinf(energy_loss) or args.energy_weight > 0.0:
         result += args.energy_weight * energy_loss
     if not math.isinf(force_loss) or args.force_weight > 0.0:
         result += args.force_weight * force_loss
+    if not math.isinf(stress_loss) or args.stress_weight > 0.0:
+        result += args.force_weight * force_loss
+
     return result
 
-def evaluate(model: torch.nn.Module,
-             accelerator: Accelerator,
-             criterion: torch.nn.Module,
+
+def evaluate(args,
+             model      : torch.nn.Module,
+             criterion  : torch.nn.Module,
              data_loader: Iterable, 
-             print_freq: int = 100, 
-             logger=None, 
-             print_progress=False, 
-             max_iter=-1):
+             max_iter   = -1):
 
     model.eval()
     criterion.eval()
 
-    loss_metrics = {'energy': AverageMeter(), 'force': AverageMeter()}
-    
-    start_time = time.perf_counter()
-    
+    loss_metrics = {
+        'total' : AverageMeter(),
+        'energy': AverageMeter(),
+        'forces': AverageMeter(),
+        'stress': AverageMeter(),
+    }
+
     with torch.no_grad():
             
         for step, data in enumerate(data_loader):
 
-            pred_y, pred_dy, _ = model(data)
+            pred_e, pred_f, pred_s = model(data)
 
-            loss_e = criterion(pred_y, data.y)
-            loss_f = criterion(pred_dy, data['force'])
-            
-            loss_metrics['energy'].update(loss_e.item(), n=pred_y .shape[0])
-            loss_metrics['force' ].update(loss_f.item(), n=pred_dy.shape[0])
-            
-            energy_loss = pred_y.detach() - data.y
-            energy_loss = torch.mean(torch.abs(energy_loss)).item()
-            loss_metrics['energy'].update(energy_loss, n=pred_y.shape[0])
-            force_loss = pred_dy.detach() - data['force']
-            force_loss = torch.mean(torch.abs(force_loss)).item()
-            loss_metrics['force'].update(force_loss, n=pred_dy.shape[0])
-            
-            if accelerator.process_index == 0:
-                # logging
-                if (step % print_freq == 0 or step == len(data_loader) - 1) and print_progress: 
-                    w = time.perf_counter() - start_time
-                    e = (step + 1) / len(data_loader)
-                    info_str = '[{step}/{length}] \t'.format(step=step, length=len(data_loader))
-                    info_str +=  'e_loss: {e_loss:.5f}, f_loss: {f_loss:.5f}, '.format(
-                        e_loss=loss_metrics['energy'].avg, f_loss=loss_metrics['force'].avg, 
-                    )
-                    info_str += 'time/step={time_per_step:.0f}ms'.format( 
-                        time_per_step=(1e3 * w / e / len(data_loader))
-                    )
-                    logger.info(info_str)
+            loss_e = None
+            loss_f = None
+            loss_s = None
+
+            if pred_e is not None:
+                loss_e = criterion(pred_e, data.y)
+            if pred_f is not None:
+                loss_f = criterion(pred_f, data['force'])
+            if pred_s is not None:
+                loss_s = criterion(pred_s, data['stress'])
+
+            loss = compute_weighted_loss(args, loss_e, loss_f, loss_s)
+
+            loss_metrics['total'].update(loss.item(), n=pred_e.shape[0])
+
+            if pred_e is not None:
+                loss_metrics['energy'].update(loss_e.item(), n=pred_e.shape[0])
+            if pred_f is not None:
+                loss_metrics['forces'].update(loss_f.item(), n=pred_f.shape[0])
+            if pred_s is not None:
+                loss_metrics['stress'].update(loss_s.item(), n=pred_s.shape[0])
             
             if ((step + 1) >= max_iter) and (max_iter != -1):
                 break
@@ -400,12 +423,25 @@ def update_best_results(args, best_metrics, val_loss, epoch):
 
     update_result = False
 
-    new_loss  = compute_weighted_loss(args, val_loss['energy'].avg, val_loss['force'].avg)
-    prev_loss = compute_weighted_loss(args, best_metrics['val_energy_loss'], best_metrics['val_force_loss'])
-    if new_loss < prev_loss:
-        best_metrics['val_energy_loss'] = val_loss['energy'].avg
-        best_metrics['val_force_loss' ] = val_loss['force'].avg
-        best_metrics['val_epoch'      ] = epoch
+    loss_new = compute_weighted_loss(args,
+            val_loss['energy'].avg,
+            val_loss['forces'].avg,
+            val_loss['stress'].avg)
+    loss_old = compute_weighted_loss(args,
+            best_metrics['val_energy_loss'],
+            best_metrics['val_forces_loss'],
+            best_metrics['val_stress_loss'])
+
+    if loss_new < loss_old:
+        if args.energy_weight > 0.0:
+            best_metrics['val_energy_loss'] = val_loss['energy'].avg
+        if args.force_weight > 0.0:
+            best_metrics['val_forces_loss'] = val_loss['forces'].avg
+        if args.stress_weight > 0.0:
+            best_metrics['val_stress_loss'] = val_loss['stress'].avg
+
+        best_metrics['val_epoch'] = epoch
+
         update_result = True
 
     return update_result
@@ -421,7 +457,12 @@ def train_one_epoch(args,
     model.train()
     criterion.train()
 
-    loss_metrics = {'total': AverageMeter(), 'energy': AverageMeter(), 'force': AverageMeter()}
+    loss_metrics = {
+        'total' : AverageMeter(),
+        'energy': AverageMeter(),
+        'forces': AverageMeter(),
+        'stress': AverageMeter(),
+    }
 
     start_time = time.perf_counter()
 
@@ -435,20 +476,35 @@ def train_one_epoch(args,
 
         e_true = data.y
         f_true = data['force']
+        s_true = data['stress']
 
-        e_pred, f_pred, _ = model(data)
+        e_pred, f_pred, s_pred = model(data)
 
-        loss_e = criterion(e_pred, e_true)
-        loss_f = criterion(f_pred, f_true)
-        loss   = compute_weighted_loss(args, loss_e, loss_f)
+        loss_e = None
+        loss_f = None
+        loss_s = None
+
+        if args.energy_weight > 0.0:
+            loss_e = criterion(e_pred, e_true)
+        if args.force_weight > 0.0:
+            loss_f = criterion(f_pred, f_true)
+        if args.stress_weight > 0.0:
+            loss_s = criterion(s_pred, s_true)
+
+        loss = compute_weighted_loss(args, loss_e, loss_f, loss_s)
 
         optimizer.zero_grad()
         accelerator.backward(loss)
         optimizer.step()
 
-        loss_metrics['total' ].update(loss  .item(), n=e_pred.shape[0])
-        loss_metrics['energy'].update(loss_e.item(), n=e_pred.shape[0])
-        loss_metrics['force' ].update(loss_f.item(), n=f_pred.shape[0])
+        loss_metrics['total'].update(loss.item(), n=e_pred.shape[0])
+
+        if args.energy_weight > 0.0:
+            loss_metrics['energy'].update(loss_e.item(), n=e_pred.shape[0])
+        if args.force_weight > 0.0:
+            loss_metrics['forces'].update(loss_f.item(), n=f_pred.shape[0])
+        if args.stress_weight > 0.0:
+            loss_metrics['stress'].update(loss_s.item(), n=s_pred.shape[0])
 
         if accelerator.process_index == 0:
 
@@ -456,22 +512,21 @@ def train_one_epoch(args,
             if step % print_freq == 0 or step == len(data_loader) - 1: 
                 w = time.perf_counter() - start_time
                 e = (step + 1) / len(data_loader)
-                info_str  = 'Epoch: [{epoch}][{step}/{length}] \t'.format(epoch=epoch, step=step, length=len(data_loader))
-                info_str += 'loss: {loss:.5f}, loss_e: {loss_e:.5f}, loss_f: {loss_f:.5f}, '.format(
-                    loss=loss_metrics['total'].avg, loss_e=loss_metrics['energy'].avg, loss_f=loss_metrics['force'].avg, 
-                )
-                info_str += 'time/step={time_per_step:.0f}ms, '.format( 
+
+                info_str_prefix  = 'Epoch [{epoch}][{step}/{length}]:\t'.format(epoch=epoch, step=step, length=len(data_loader))
+                info_str_postfix = 'time/step={time_per_step:.0f}ms, '.format(
                     time_per_step=(1e3 * w / e / len(data_loader))
                 )
-                info_str += 'lr={:.2e}'.format(optimizer.param_groups[0]["lr"])
-                logger.info(info_str)
+                info_str_postfix += 'lr={:.2e}'.format(optimizer.param_groups[0]["lr"])
+
+                log_metrics(args, logger, info_str_prefix, info_str_postfix, loss_metrics)
 
     return loss_metrics
 
 def _train(args):
     
-    _log = FileLogger(is_master=True, is_rank0=True, output_dir=args.output_dir)
-    _log.info(args)
+    logger = FileLogger(is_master=True, is_rank0=True, output_dir=args.output_dir)
+    logger.info(args)
     
     # since dataset needs random 
     torch.manual_seed(args.seed)
@@ -487,8 +542,8 @@ def _train(args):
     model = EquiformerV2_OC20(
         # First three arguments are not used
         None, None, None,
-        compute_forces   = True,
-        compute_stress   = False,
+        compute_forces   = args. force_weight > 0.0,
+        compute_stress   = args.stress_weight > 0.0,
         max_radius       = r_max,
         max_num_elements = 95,
         alpha_drop       = args.alpha_drop,
@@ -497,13 +552,13 @@ def _train(args):
     )
 
     if args.load_checkpoint_model is not None:
-        _log.info(f'Loading model checkpoint {args.load_checkpoint_model}...')
+        logger.info(f'Loading model checkpoint {args.load_checkpoint_model}...')
         model.load_state_dict(torch.load(args.load_checkpoint_model))
 
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
 
     if accelerator.process_index == 0:
-        _log.info('Number of params: {}'.format(n_parameters))
+        logger.info('Number of params: {}'.format(n_parameters))
     
     ''' Optimizer and LR Scheduler '''
     optimizer = create_optimizer(args, model)
@@ -517,18 +572,19 @@ def _train(args):
     model, optimizer, lr_scheduler = accelerator.prepare(model, optimizer, lr_scheduler)
 
     if args.load_checkpoint is not None:
-        _log.info(f'Loading checkpoint {args.load_checkpoint}...')
+        logger.info(f'Loading checkpoint {args.load_checkpoint}...')
         accelerator.load_state(args.load_checkpoint)
 
     ''' Compute stats '''
     if args.compute_stats:
-        compute_stats(train_loader, max_radius=args.radius, logger=_log, print_freq=args.print_freq)
+        compute_stats(train_loader, max_radius=args.radius, logger=logger, print_freq=args.print_freq)
         return
     
     # record the best validation and testing loss and corresponding epochs
     best_metrics = {'val_epoch': 0, 'test_epoch': 0, 
-         'val_force_loss': float('inf'),  'val_energy_loss': float('inf'), 
-        'test_force_loss': float('inf'), 'test_energy_loss': float('inf')}
+         'val_energy_loss': float('inf'),  'val_forces_loss': float('inf'),  'val_stress_loss': float('inf'),
+        'test_energy_loss': float('inf'), 'test_forces_loss': float('inf'), 'test_stress_loss': float('inf'),
+    }
 
     for epoch in range(args.epochs):
         
@@ -538,41 +594,40 @@ def _train(args):
 
         train_loss = train_one_epoch(args=args, model=model, accelerator=accelerator, criterion=criterion,
             data_loader=train_loader, optimizer=optimizer,
-            epoch=epoch, print_freq=args.print_freq, logger=_log)
+            epoch=epoch, print_freq=args.print_freq, logger=logger)
         
-        val_loss = evaluate(model=model, accelerator=accelerator, criterion=criterion, 
-            data_loader=val_loader,
-            print_freq=args.print_freq, logger=_log, print_progress=False)
+        val_loss = evaluate(model=model, criterion=criterion, data_loader=val_loader)
         
         # Only main process should save model
         if accelerator.process_index == 0:
 
             update_val_result = update_best_results(args, best_metrics, val_loss, epoch)
+
             if update_val_result:
                 accelerator.save_state(
                     os.path.join(args.output_dir,
-                        'best_val_epochs@{}_e@{:.4f}_f@{:.4f}'.format(epoch, val_loss['energy'].avg, val_loss['force'].avg)),
+                        'best_val_epochs@{}_e@{:.4f}'.format(epoch, val_loss['total'].avg)),
                         safe_serialization=False)
 
-            info_str = 'Epoch: [{epoch}] train_e_loss: {train_e_loss:.5f}, train_f_loss: {train_f_loss:.5f}, '.format(
-                epoch=epoch, train_e_loss=train_loss['energy'].avg, train_f_loss=train_loss['force'].avg)
-            info_str += 'val_e_loss: {:.5f}, val_f_loss: {:.5f}, '.format(val_loss['energy'].avg, val_loss['force'].avg)
-            info_str += 'Time: {:.2f}s'.format(time.perf_counter() - epoch_start_time)
-            _log.info(info_str)
+            info_str_prefix  = 'Epoch: [{epoch}] Test:\t'.format(epoch=epoch)
+            info_str_postfix = 'Time: {:.2f}s'.format(time.perf_counter() - epoch_start_time)
 
-            info_str  = 'Best -- val_epoch={}, test_epoch={}, '.format(best_metrics['val_epoch'], best_metrics['test_epoch'])
-            info_str +=  'val_e_loss: {:.5f}, val_f_loss: {:.5f}, '.format(best_metrics['val_energy_loss'], best_metrics['val_force_loss'])
-            _log.info(info_str)
+            log_metrics(args, logger, info_str_prefix, info_str_postfix, train_loss)
+
+            info_str_prefix  = 'Epoch: [{epoch}] Val :\t'.format(epoch=epoch)
+            info_str_postfix = None
+
+            log_metrics(args, logger, info_str_prefix, info_str_postfix, train_loss)
 
     if test_loader is not None:
         # evaluate on the whole testing set
-        test_loss = evaluate(model=model, accelerator=accelerator, criterion=criterion, 
-            data_loader=test_loader,
-            print_freq=args.print_freq, logger=_log, print_progress=False, max_iter=-1)
+        test_loss = evaluate(model=model, criterion=criterion, data_loader=test_loader)
  
-        info_str  = 'Test -- '
-        info_str += 'test_e_loss: {:.5f}, test_f_loss: {:.5f}'.format(test_loss['energy'].avg, test_loss['force'].avg)
-        _log.info(info_str)
+        info_str_prefix  = 'Test:\t'
+        info_str_postfix = None
+
+        log_metrics(args, logger, info_str_prefix, info_str_postfix, test_loss)
+
 
 def train(args):
 
@@ -584,6 +639,9 @@ def train(args):
         raise ValueError("--statistics-file is a required argument")
     if args.output_dir is None:
         raise ValueError("--output-dir is a required argument")
+
+    if args.energy_weight == 0.0 and args.force_weight == 0.0 and args.stress_weight == 0.0:
+        raise ValueError("at least one non-zero loss weight is required")
 
     if args.output_dir:
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
