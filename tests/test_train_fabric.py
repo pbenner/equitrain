58d67984fcaa4b5b41720201a9643ff0a2726a34
carlosmada22 <trollmada@gmail.com>
2024-10-21 11:38:15 +0200

    Added fabric train script

---
+++ tests/test_train_fabric.py
@@ -0,0 +1,27 @@
+# %%
+        
+from equitrain import get_args_parser_train
+from equitrain import train_fabric
+
+# %%
+
+def main():
+
+    r = 4.5
+
+    args = get_args_parser_train().parse_args()
+
+    args.train_file      = f'tests/data/train.h5'
+    args.valid_file      = f'tests/data/valid.h5'
+    args.statistics_file = f'tests/data/statistics.json'
+    args.output_dir      = 'tests/result'
+
+    args.epochs     = 10
+    args.batch_size = 64
+    args.lr         = 0.01
+
+    train_fabric(args)
+
+# %%
+if __name__ == "__main__":
+    main()
