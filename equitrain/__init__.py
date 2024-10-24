f63b2a274ed46846d0eb00e41111c7444f5a4f20
carlosmada22 <trollmada@gmail.com>
2024-10-24 17:04:16 +0200

    Added train script with Lightning Fabric

--- equitrain/__init__.py
+++ equitrain/__init__.py
@@ -1,6 +1,7 @@
 
 from .argparser import get_args_parser_preprocess, get_args_parser_train, get_args_parser_test, get_args_parser_predict
 from .train import train
+from .train_lightning import train_lightning
 from .train_fabric import train_fabric
 from .predict import predict, predict_atoms, predict_structures, predict_graphs
 from .preprocess import preprocess
